7833ea0a514a956e83b1962262577dedd82d66b3
Tahir <coskun.dincer2024@gmail.com>
2024-11-20 04:33:53 +0300

    ' ' First ' init File ' structure ' '

---
+++ src/data.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    question: " asdasdasd? ",
+    answer: " asdasdasd ",
+    id: 1,
+  },
+  {
+    question: " asdasdasd? ",
+    answer: " asdasdasd ",
+    id: 2,
+  },
+  {
+    question: " asdasdasd? ",
+    answer: " asdasdasd ",
+    id: 3,
+  },
+  {
+    question: " asdasdasd? ",
+    answer: " asdasdasd ",
+    id: 4,
+  },
+];
